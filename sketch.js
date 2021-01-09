7f5dee7c1e08df26fc27ae7f2e4b07dd575faabc
Ishan1818 <72986416+Ishan1818@users.noreply.github.com>
2021-01-09 13:23:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var dub;
+var paper1;
+var ground;
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1000, 700);
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	
+	//Create the Bodies Here.
+	ground=new Ground(width/2,670,width,20);
+	
+	//rec1 = createSprite(830, 630, 10, 60  );
+
+	//rec2 = createSprite(880, 655, 90, 10  );
+   
+	
+	//rec3 = createSprite (920, 630, 10, 60 );
+	paper1=new paper(200, 450, 50)
+
+	dub=new dustbin(830, 660)
+	
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+
+
+  background("white");
+ 
+ paper1.display()
+ ground.display()
+ dub.display()
+ drawSprites();
+}
+function keyPressed() {
+	if (keyCode === 32) {
+
+	  Matter.Body.applyForce(paper1.body,paper1.body.position,{x:33,y:-35});
+  
+	}
+}
+
+
+
